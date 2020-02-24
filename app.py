c77d543f564bac6d5c8b025a19dc10c568d07871
Brendan Mchugh <bmchugh@redhat.com>
2020-02-24 15:41:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class RH(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        message = ['Hi David ' + self.address_string() + '<br>\n',
+        message = ['Hi ' + self.address_string() + '<br>\n',
             'Date: ' + self.date_time_string() + '<br>\n']
         for i in message:
             self.wfile.write(bytes(i, "utf8"))
